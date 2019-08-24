19c309dae1267e993cecccb32ddc6cc5d1f2712b
csamatov96 <996bkg@gmail.com>
2019-08-24 11:12:39 -0500

    git added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,4 @@
 node {
     stage("Say Hello")
-    properties([parameters([text(defaultValue: 'Chyngyz', description: 'give me ur name ', name: 'NAME')]), pipelineTriggers([cron('* * * * *')])])
-    sh "echo Hello ${NAME}"
+    git 'https://github.com/csamatov96/jenkins_pipeline.git'
 }
