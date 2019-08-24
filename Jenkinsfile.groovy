af1c873b457f75b38386494b28f28300954177c8
csamatov96 <996bkg@gmail.com>
2019-08-24 11:24:02 -0500

    pipeline syntax added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,7 @@
 node {
     stage("Say Hello")
+    properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
+                                                                                QA: 3.15.44.42 
+                                PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
     git 'https://github.com/csamatov96/jenkins_pipeline.git'
 }
