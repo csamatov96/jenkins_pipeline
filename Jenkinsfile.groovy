380416515835ce1bd75c56857faf7142123428e9
csamatov96 <996bkg@gmail.com>
2019-08-24 12:01:23 -0500

    git changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,23 +1,18 @@
 node {
-    stage("Pull Repo")
-    properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
-                                                                                 QA: 3.15.44.42 
-                                PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
-    git 'https://github.com/farrukh90/jenkins_april.git'
-    
+    stage("Pull Repo"){
+        properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev:3.15.153.106 QA:3.15.44.42 Prod:18.191.19.171''', name: 'Remote_instance', trim: false)])])
+        git 'https://github.com/leventelibal/jenkins_repo.git'
+    }
     stage("Install Apache"){
-        sh "ssh  ec2-user@${Remote_instances}     sudo yum install httpd -y"
-    
+        sh "ssh   centos@${Remote_instance}    sudo yum install httpd -y"
+    }
     stage("Create Index.html"){
-        sh "scp index.html ec2-user@${Remote_instances}:/tmp"
+        sh "scp  index.html  centos@${Remote_instance}:/tmp"
     }
-    stage("Move files"){
-        sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/html"
+    stage("Move Files"){
+        sh "ssh   centos@${Remote_instance}    sudo mv /tmp/index.html  /var/www/html/index.html"
     }
-    stage("Restart httpd") {
-        sh "ssh ec2-user@${Remote_intances} sudo systemctl restart httpd"
-    }
-
-
+    stage("Restart httpd"){
+        sh "ssh   centos@${Remote_instance} sudo systemctl restart  httpd"
     }
 }
