940babd73f148d764279e9e8b47c4e2d4f8e5c30
csamatov96 <996bkg@gmail.com>
2019-08-24 11:28:39 -0500

    ssh + Var added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,11 @@
 node {
-    stage("Say Hello")
+    stage("Pull Repo")
     properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
                                                                                 QA: 3.15.44.42 
                                 PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
     git 'https://github.com/csamatov96/jenkins_pipeline.git'
+    stage("Install Apache"){
+        sh "ssh     ec@user@${Remote_instances}     sudo yum install httpd -y"
+
+    }
 }
