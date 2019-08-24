544e24781a27c71d7590035ac816ba1208e7b227
csamatov96 <996bkg@gmail.com>
2019-08-24 11:46:15 -0500

    git changed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
     properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
                                                                                  QA: 3.15.44.42 
                                 PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
-    git 'https://git@github.com:farrukh90/jenkins_april.git'
+    git 'https://github.com/csamatov96/jenkins_pipeline.git'
     
     stage("Install Apache"){
         sh "ssh  ec2-user@${Remote_instances}     sudo yum install httpd -y"
