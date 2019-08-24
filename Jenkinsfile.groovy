9a60a50cbdee015bfb08ebe540e53a25cf9e5abd
csamatov96 <996bkg@gmail.com>
2019-08-24 11:43:55 -0500

    https:// added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
     properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
                                                                                  QA: 3.15.44.42 
                                 PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
-    git 'git@github.com:farrukh90/jenkins_april.git'
+    git 'https://git@github.com:farrukh90/jenkins_april.git'
     
     stage("Install Apache"){
         sh "ssh  ec2-user@${Remote_instances}     sudo yum install httpd -y"
@@ -12,7 +12,7 @@ node {
         sh "scp index.html ec2-user@${Remote_instances}:/tmp"
     }
     stage("Move files"){
-        sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/"
+        sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/html"
     }
     stage("Restart httpd") {
         sh "ssh ec2-user@${Remote_intances} sudo systemctl restart httpd"
