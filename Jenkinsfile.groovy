db9cfe8117a2dd37987928276977768d478cf791
csamatov96 <996bkg@gmail.com>
2019-08-24 11:59:12 -0500

    changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,13 @@ node {
     properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
                                                                                  QA: 3.15.44.42 
                                 PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
-    git 'https://github.com/csamatov96/jenkins_pipeline.git'
+    git 'https://github.com/farrukh90/jenkins_april.git'
     
     stage("Install Apache"){
         sh "ssh  ec2-user@${Remote_instances}     sudo yum install httpd -y"
     
     stage("Create Index.html"){
-        sh "echo 'Hi' > index.html ec2-user@${Remote_instances}:/tmp"
+        sh "scp index.html ec2-user@${Remote_instances}:/tmp"
     }
     stage("Move files"){
         sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/html"
