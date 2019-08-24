aded893763b63b872650d5b6516a561daa83f350
csamatov96 <996bkg@gmail.com>
2019-08-24 11:42:31 -0500

    changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 node {
     stage("Pull Repo")
     properties([parameters([string(defaultValue: '3.15.153.106', description: '''Dev: 3.15.153.106
-                                                                                QA: 3.15.44.42 
+                                                                                 QA: 3.15.44.42 
                                 PROD: 18.191.19.171''', name: 'Remote_instances', trim: true)])])
     git 'git@github.com:farrukh90/jenkins_april.git'
     
@@ -12,7 +12,7 @@ node {
         sh "scp index.html ec2-user@${Remote_instances}:/tmp"
     }
     stage("Move files"){
-        sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/index.html"
+        sh "ssh ec2-user@${Remote_instances}    sudo mv /tmp/index.html     var/www/html/"
     }
     stage("Restart httpd") {
         sh "ssh ec2-user@${Remote_intances} sudo systemctl restart httpd"
